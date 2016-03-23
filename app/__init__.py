bc869dc195d9d4cf469e774776ea0d2f4bd84153
EC2 Default User <ec2-user@ip-172-31-37-116.us-west-2.compute.internal>
2016-03-23 16:13:24 +0000

    new urls

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+	
+@app.route("/sessionLogin")
+def sessionLogin():
+    return render_template('sessionLogin.html')
+	
+@app.route("/stationOverview")
+def stationOverview():
+    return render_template('stationOverview.html')
+	
+@app.route("/stationQuestion")
+def stationQuestion():
+    return render_template('stationQuestion.html') 
+	
+@app.route("/stationVideo")
+def stationVideo():
+    return render_template('stationVideo.html')
+	
+@app.route("/template")
+def template():
+    return render_template('template.html')
 
 if __name__ == "__main__":
     app.run()
