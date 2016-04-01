632701e72e7be965bcbfbcb58c71706b562ddc8a
EC2 Default User <ec2-user@ip-172-31-37-116.us-west-2.compute.internal>
2016-04-01 20:40:40 +0000

    app login

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
+from user import User
+from flask.ext.pymongo import PyMongo
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'sapQR'
+mongo = PyMongo(app, config_prefix="MONGO")
 
 @app.route("/")
 def index():
     return render_template('index.html')
 	
+@app.route("/login")
+def login():
+	with app.app_context():
+		return  mongo.db.users.find_one({'username': "1337"})['username']
+	
 @app.route("/sessionLogin")
 def sessionLogin():
+	
     return render_template('sessionLogin.html')
 	
 @app.route("/stationOverview")
@@ -27,4 +38,4 @@ def template():
     return render_template('template.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
