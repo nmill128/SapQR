86fd97266eb48fffbb3e6854f969cb54ed7415bc
EC2 Default User <ec2-user@ip-172-31-37-116.us-west-2.compute.internal>
2016-04-19 02:26:24 +0000

    user auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,55 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for, flash
-from user import User
+from flask import render_template, request, redirect, url_for, flash, session
+from user import User, LoginForm, CreateLoginForm
+from werkzeug.debug import DebuggedApplication
 from flask.ext.pymongo import PyMongo
 app = Flask(__name__)
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
+app.secret_key = "claiisbestlai"
 app.config['MONGO_DBNAME'] = 'sapQR'
+app.config['DEBUG'] = True
 mongo = PyMongo(app, config_prefix="MONGO")
 
 @app.route("/")
 def index():
     return render_template('index.html')
 	
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	with app.app_context():
-		return  mongo.db.users.find_one({'username': "1337"})['username']
+	form = LoginForm(request.form)
+	if request.method == "POST":
+		if form.validate():
+			session['user_id'] = form.user.get_id()
+			return redirect(url_for('loggedIn', next=request.url))
+		return "validate fail"
+	return render_template('login.html', form=form)
+			
 	
-@app.route("/sessionLogin")
+@app.route("/loggedIn")
+def loggedIn():
+	if session['user_id']:
+		uname = mongo.db.users.find_one({'username': session['user_id']})["username"]
+		
+		return u'<h1>logged in as %s</h1><a href="/logout">logout</a>' % uname
+	else:
+		return redirect(url_for('login', next=request.url))
+	
+@app.route("/logout")
+def logout():
+	session['user_id'] = None
+	return redirect(url_for('login', next=request.url))
+	
+@app.route("/sessionLogin", methods=["GET", "POST"])
 def sessionLogin():
-	
-    return render_template('sessionLogin.html')
-	
+	form = CreateLoginForm(request.form)
+	if request.method == "GET":
+		counter = mongo.db.counters.find_and_modify(query={'_id': 'username'}, update={ "$inc": { "seq": 1 } }, upsert=False, fullresponse=True)
+		return render_template('sessionLogin.html', username=counter["seq"], form=form)
+	if form.validate():
+		session['user_id'] = form.user.get_id()
+		return u'<h2>Annonyamous user created:</h2><h1>%s</h1><h2>Please screen shot your userid in case you are logged out</h2>' % session['user_id']
+	return "validate fail"
 @app.route("/stationOverview")
 def stationOverview():
     return render_template('stationOverview.html')
@@ -58,4 +87,4 @@ def stationInfo():
     return render_template('stationInfo.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)#,use_debugger=True)
--- app/user.py
+++ app/user.py
@@ -1,6 +1,11 @@
 
+from wtforms import Form, TextField, PasswordField, validators, RadioField, Field, IntegerField
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask.ext.pymongo import PyMongo
+import app
 
-from werkzeug.security import check_password_hash
+
+
 
 class User():
 
@@ -21,4 +26,67 @@ class User():
 
     @staticmethod
     def validate_login(password_hash, password):
-        return check_password_hash(password_hash, password)
+        return password_hash == password
+        #return check_password_hash(password_hash, password)
+
+	def hash_password(password):
+		return generate_password_hash(password)
+		
+class LoginForm(Form):
+	username = TextField('Username', [validators.Required()])
+	password = PasswordField('Password', [validators.Required()])
+	
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None	
+		
+	def validate(self):
+		rv = Form.validate(self)
+		if not rv:
+			return False
+		userEntry = app.mongo.db.users.find_one({'username': self.username.data})
+        #user = User.query.filter_by(
+        #    username=self.username.data).first()
+		if userEntry is None:
+			self.username.errors.append('Unknown username')
+			return False
+		user = User(userEntry["username"])
+
+		if not user.validate_login(userEntry["password"], self.password.data):
+			self.password.errors.append('Invalid password')
+			return False
+
+		self.user = user
+		return True
+		
+class CreateLoginForm(Form):
+	username = TextField('Username', [validators.Required()])
+	password1 = PasswordField('Password1', [validators.Required()])
+	password2 = PasswordField('Password2', [validators.Required()])
+	gender = Field('Gender', [validators.Required()])
+	age = IntegerField('Age', [validators.Required()])
+	
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None	
+		
+	def validate(self):
+		rv = Form.validate(self)
+		if not rv:
+			return False
+		if not self.password1.data == self.password2.data:
+			return False
+		userEntry = app.mongo.db.users.find_one({'username': self.username.data})
+		if userEntry:
+			return False
+		doc =  {'username': self.username.data,
+				'gender':self.gender.data,
+				'age':self.age.data,
+				'password': generate_password_hash(self.password1.data)}
+		try:
+			app.mongo.db.users.insert(doc)
+		except pymongo.errors.DuplicateKeyError as e:
+			return False
+		user = User(self.username.data)
+		self.user = user
+		return True
